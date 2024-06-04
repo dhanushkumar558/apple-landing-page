2ee3c5fbf539377cb4fa8733a1081513ed81b064
dhanushkumar558 <dhanushkumar558@hotmail.com>
2024-06-04 15:04:08 +0530

    initialized

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDom from "react-dom";
+import "./App.jsx";
+import Bootstrappractice from "./App.jsx";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"))
+root.render(<Bootstrappractice />)
